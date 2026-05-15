31f06e78c89e50d42a8661d2dd78d0ef5ff1c2a3
Daniil Sochnev <sochnevde09@gmail.com>
2026-05-15 07:55:00 +0500

    fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import AdminPage from './pages/AdminPage'
 export default function App() {
   return (
     <Routes>
-      <Route path="/edu_table" element={<AdminPage department="Educational" />} />
-      <Route path="/csd_table" element={<AdminPage department="CSD" />} />
-      <Route path="/hostel_table" element={<AdminPage department="hostel" />} />
-      <Route path="/edu" element={<EducationalPage />}/>
-      <Route path="/csd" element={<CSDPage />}/>
-      <Route path="/hostel" element={<HostelPage />}/>
+      <Route path="/spravki/edu_table" element={<AdminPage department="Educational" />} />
+      <Route path="/spravki/csd_table" element={<AdminPage department="CSD" />} />
+      <Route path="/spravki/hostel_table" element={<AdminPage department="hostel" />} />
+      <Route path="/spravki/edu" element={<EducationalPage />}/>
+      <Route path="/spravki/csd" element={<CSDPage />}/>
+      <Route path="/spravki/hostel" element={<HostelPage />}/>
     </Routes>
   )
 }
